214574ac9d98d16fd92ca5081eeda39890f7e542
Lee <lee@kezzi.co>
2023-01-25 20:02:51 -0800

    Initial Commit

---
+++ falling letters/falling_lettersApp.swift
@@ -0,0 +1,17 @@
+//
+//  falling_lettersApp.swift
+//  falling letters
+//
+//  Created by lee on 1/25/23.
+//
+
+import SwiftUI
+
+@main
+struct falling_lettersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
